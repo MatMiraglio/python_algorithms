cf0d302793f863f8c474389124a38ee030db5a73
Matias <miragliomatias@gmail.com>
2019-04-06 18:44:08 +0200

    working on better subpalindrome algorithm

--- src/Strings/subpalindrome.py
+++ src/Strings/subpalindrome.py
@@ -1,5 +1,40 @@
 from is_palindrome import is_palindrome
 
+def longest_palindrome(s: str) -> str:
+
+    L, R = 0, 0
+    max_len = 0
+
+    for i in range(len(s)):
+        left1, right1 = _expand(s, i, i)
+        left2, right2 =_expand(s, i, i + 1)
+        len1 = right1 - left1
+        len2 = right2 - left2
+
+        if len1 > len2 and len1 > max_len:
+            L, R = left1, right1
+        elif len2 > len1 and len2 > max_len:
+            L, R = left2, right2
+
+
+
+    return s[L:R]
+
+
+def _expand(string, left, right):
+    L = left
+    R = right
+
+    while left >= 0 and right < len(string) - 1:
+        if string[left] == string[right]:
+            right += 1
+            left -= 1
+        else:
+            break
+    
+    return left - right - 1 
+
+
 
 def longestPalindrome(s: str) -> str:
     start = end = 0
@@ -18,6 +53,13 @@ def longestPalindrome(s: str) -> str:
         start += 1
 
     return sub_palindrome
+
+assert longest_palindrome("aba") == "aba"
+assert longest_palindrome("ababa") == "ababa"
+assert longest_palindrome("asdfdg") == "dfd"
+assert longest_palindrome("qwertyuy") == "yuy"
+assert longest_palindrome("qwerrtyp") == "rr"
+assert longest_palindrome("qwerrewq") == "qwerrewq"
 
 
 assert longestPalindrome("aba") == "aba"
