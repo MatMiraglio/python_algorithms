fbdb9fe38294fd14f321e8fcb7112ffdc5bd3a8e
Matias <miragliomatias@gmail.com>
2019-04-08 17:56:49 +0200

    fix imports and test cases

--- test/test_others.py
+++ test/test_others.py
@@ -1,4 +1,4 @@
-from ..src.inplace_swap import swap
+from ..src.others.inplace_swap import swap
 import unittest
 
 
@@ -7,8 +7,8 @@ class Test(unittest.TestCase):
     def test_swap(self):
         test_cases = [
             ([1, 2, 3, 4, 5], 1, 4, [1, 5, 3, 4, 2]),
-            ([1, 2, 3, 4, 5], 1, 4, [1, 5, 3, 4, 2]),
-            ([1, 2, 3, 4, 5], 1, 4, [1, 5, 3, 4, 2])
+            ([1, 2, 3, 4, 5], 0, 1, [2, 1, 3, 4, 5]),
+            ([1, 2, 3, 4, 5], 3, 4, [1, 2, 3, 5, 4])
         ]
         for case in test_cases:
             array, index1, index2, expected = case
